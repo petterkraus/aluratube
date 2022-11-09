fdcc0f0d103dbf5466d0250c9987c8f949702fa2
Petter Kraus <petterkraus@gmail.com>
2022-11-09 11:57:10 -0300

    Terceira aula completa

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,16 @@
 import { useState } from "react";
 import styled from "styled-components";
 import config from "../config.json";
-import { CSSReset } from "../src/components/CSSReset";
+
 import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/Timeline";
-import Banner from "../src/components/Banner";
+import Banner from "../src/components/Menu/components/Banner";
 
 export default function HomePage() {
   const [search, setSearch] = useState("");
 
   return (
     <>
-      <CSSReset />
       <div
         style={{
           display: "flex",
@@ -31,6 +30,7 @@ export default function HomePage() {
 }
 
 const StyledHeader = styled.div`
+  background-color: ${({ theme }) => theme.backgroundLevel1};
   img {
     width: 80px;
     height: 80px;
@@ -49,7 +49,6 @@ function Header() {
   return (
     <StyledHeader>
       <Banner />
-      {/* <img src="" alt="" /> */}
       <section className="user-info">
         <img
           src={`https://github.com/${config.github}.png`}
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import DarkModeSwitch from "./components/DarkModeSwitch";
 import Search from "./components/Search";
 
 const StyledMenu = styled.header`
@@ -32,6 +33,7 @@ export default function Menu({ search, setSearch }) {
         <Logo />
       </div>
       <Search search={search} setSearch={setSearch} />
+      <DarkModeSwitch />
     </StyledMenu>
   );
 }
